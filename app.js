aef35f0b1dc262a943f28f0cbb60bc8e97ad8be8
Kostis Grf <kostisgratsos@hotmail.gr>
2022-05-21 09:19:58 +0300

    Updates app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ async function getPharmacies (lon,lat){
         // const siteUrl=`https://www.vrisko.gr/en/pharmacy-duties/near-me/?lon=${lon}&lat=${lat}`;
         const siteUrl='https://www.vrisko.gr/'
         const res=await axios({
-            method:"POST",
+            method:"PUT",
             url:siteUrl,
             headers:{
                 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'}
